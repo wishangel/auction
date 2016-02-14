4f228264b5e6ffd64d7f1c60597bad97082bb195
Akihiko Yamagata <wishangel0126@gmail.com>
2016-02-14 16:50:29 +0900

    index 周りの表示セット

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/items/:id' => 'items#show'
+  get '/items/:id' => 'items#show', as: :item
+  get '/items' => 'items#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
