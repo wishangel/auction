5ae57b375ff3962d20a3db4f366fcad2dc1855c8
Akihiko Yamagata <wishangel0126@gmail.com>
2016-02-14 18:41:42 +0900

    編集ロジック　更新ロジックの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get '/items/new' => 'items#new'
   get '/items/:id' => 'items#show', as: :item
+  patch '/items/:id' => 'items#update'
+  get '/items/:id/edit' => 'items#edit'
   get '/items' => 'items#index'
+  post '/items' => 'items#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
